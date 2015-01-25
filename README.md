91ecfe493fdf084a37d6b537c73081adcc5ff9bf
Heather <heather.dykstra@colorado.edu>
2015-01-25 13:16:13 -0700

    Added Versioning
    added a block of code with our names and the versions

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ A Proof of Concept for JPL
 
 Chris Acuna, Heather Dykstra, Sierra Flynn, Semere Ghebrecristos, Hope Sanford, Josh Weaver
 
-Version 2.0
+Version 2.01
+1/25/2015
--- client/main.js
+++ client/main.js
@@ -1,3 +1,14 @@
+/*
+Collaborative Campaign Tool
+GRAVITEAM by SPACEBUFFS
+
+Chris Acuna, Heather Dykstra, Sierra Flynn, Semere Ghebrecristos, Hope Sanford, Josh Weaver
+
+main.js is the file where we keep track of our functions
+
+Version 2.01
+1/25/2015
+*/
 //--------------------------------------------------------------
 /*
   Template.login.events({
