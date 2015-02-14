0b1ba0eb3b8082d1697290076c8c780c6c547263
ghebrecs <ghebrecs@colorado.edu>
2015-02-14 08:56:41 -0700

    back to what it was

---
+++ route.js
@@ -0,0 +1,46 @@
+/*
+
+
+
+Router.configure({    
+	
+	model3dTemplate: 'model3d',
+	chatTemplate: 'chat',
+	whiteboardTemplate: 'whiteboard',
+	timelineTemplate: 'timeline',
+	notFoundTemplate: 'notFound',
+    loadingTemplate: 'loading',
+    profileTemplate: 'profile'
+
+});
+
+Router.map(function() {
+
+    this.route('3D Model', { 
+           path: '/model3d',        
+           template: 'model3d',    
+     });
+
+    this.route('Chat', { 
+           path: '/chat',        
+           template: 'chat',    
+     });
+
+    this.route('Whiteboard', { 
+           path: '/whiteboard',        
+           template: 'whiteboard',    
+     });
+
+    this.route('Timeline Event', { 
+           path: '/timeline',        
+           template: 'timeline'    
+     });
+
+	this.route('my profile', { 
+           path: '/profile',        
+           template: 'profile'    
+     });
+
+});
+
+*/
