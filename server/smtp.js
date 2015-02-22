1eec53d62d054d8c7b46cd8dca25821da274cea5
cracuna <chris.acuna@colorado.edu>
2015-02-22 15:24:32 -0700

    email.js

--- server/smtp.js
+++ server/smtp.js
@@ -1,12 +1,12 @@
 // server/smtp.js
 Meteor.startup(function () {
 	/* smtp = {
-    	email: 'your_email',   // eg: server@gentlenode.com
-    	password: 'your_password',   // eg: 3eeP1gtizk5eziohfervU
-    	server:   'smtp.gmail.com',  // eg: mail.gandi.net
+    	email: 'your_email',   // eg: yourname@email.com
+    	password: 'your_password',   // eg: izk5eziohfervU
+    	server:   'smtp.gmail.com',  // eg: mail.email.net
     	port: 25
-  	}
+  	}*/
 
-  process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port; */
-  process.env.MAIL_URL = 'smtp://your_username:your_password@smtp.gmail.com:25';
-});
+  process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.email) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port; 
+  //process.env.MAIL_URL = 'smtp://your_username:your_password@smtp.gmail.com:25';
+});Meteor
