d037af6e9ac1c28adc3b8b0a7b6d72ca8c12cff5
cracuna <chris.acuna@colorado.edu>
2015-02-23 20:07:29 -0700

    syntax error

--- server/smtp.js
+++ server/smtp.js
@@ -1,12 +1,12 @@
 // server/smtp.js
 Meteor.startup(function () {
-	smtp = {
-    	email: 'cracuna2@gmail.com',   // eg: yourname@email.com
-    	password: '69592onixz2',   // eg: izk5eziohfervU
+/*	smtp = {
+    	email: 'your_username',   // eg: yourname@email.com
+    	password: 'your_password',   // eg: izk5eziohfervU
     	server:   'smtp.gmail.com',  // eg: mail.email.net
     	port: 25
   	}
 
   process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.email) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port; 
-  //process.env.MAIL_URL = 'smtp://your_username:your_password@smtp.gmail.com:25';
+ */process.env.MAIL_URL = 'smtp://your_username:your_password@smtp.gmail.com:25';
 });
