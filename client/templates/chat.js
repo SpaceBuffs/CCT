13e48397954a903a9189da1f87831d80fe93bb01
czarcasticjack <jowe2207@localhost.(none)>
2015-03-12 18:53:39 -0600

    Prettied chat timestamp

--- client/templates/chat.js
+++ client/templates/chat.js
@@ -6,10 +6,12 @@
 Template.chat.events({
 	"submit .new-chatMessage": function(event){
 	var chatMessage = event.target.chatMessage.value;
-	
+	var date = new Date();
+        
 	ChatModel.insert({
 		chatMessage : chatMessage,
-		createdAt : new Date(),
+		createdAtTime : date.toLocaleTimeString(),
+                createdAtDate : date.toLocaleDateString(),
                 user_name : Meteor.user().username
         });
 
