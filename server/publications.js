dde71f415e9eac242b3bbdc779e5426cb856b73c
hopesanford <hope.sanford@colorado.edu>
2015-04-04 12:35:20 -0600

    remove commented out stuff

--- server/publications.js
+++ server/publications.js
@@ -12,32 +12,12 @@ Meteor.publish('ChatModel', function () {
    return ChatModel.find({}); 
 }); 
 
-/*Meteor.publish(null, function() {
-    if(this.userID) {
-        return Meteor.users.find({_id: this.userID},
-                                 {fields: {'isAdmin':1}});
-    }
-});*/
 
 Meteor.publish("userData", function () {
   return Meteor.users.find({_id: this.userId},
                            {fields: {'isAdmin': 1, 'missions':1, 'name':1}});
 });
-/*
-Meteor.publish("userData", function() {
-    //if(this.userID) {
-        return Meteor.users.find({_id: this.userId},
-                                {fields: {'missions':1}});
-    //}
-});
 
-Meteor.publish("userData", function() {
-   // if(this.userID) {
-        return Meteor.users.find({_id: this.userId},
-                                {fields: {'name':1}});
-    //}
-});
-*/
 Meteor.users.allow({
     update: function(userId, doc){
         return doc._id === userId; // can update their own profile
