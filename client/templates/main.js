0700fecf2a9db4776efe0e8e27035972753b51df
hopesanford <hope.sanford@colorado.edu>
2015-04-10 17:30:16 -0600

    code clean up

--- client/templates/main.js
+++ client/templates/main.js
@@ -11,29 +11,7 @@ Version 2.01
 1/25/2015
 */
 //--------------------------------------------------------------
-/*
-  Template.login.events({
-    'submit login-form' : function(e, t){
-      e.preventDefault();
-      // retrieve the input field values
-      var emailVar = t.find('#login-email').value;
-      var passwordVar = t.find('#login-password').value;
-        // Trim and validate your fields here.... 
-        // If validation passes, supply the appropriate fields to the
-        // Meteor.loginWithPassword() function.
-        Meteor.loginWithPassword(emailVar, passwordVar);
-     }
-  });
-*/
-/*
-Template.graviteam.logged_in = function(){
-  return Meteor.user();
-}
-*/
-//Template.timeline.activities = function(){
-//  return ActivitiesModel.find({});
-//} ***moved to global.js. The below may need to be moved there too.
-// Shouldn't this be placed in timeline.js?
+
 
 Template.timeline.events({
   'click #new_activity': function() {
